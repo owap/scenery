c6473e306a5ec41f25ce56c7476797082a4a8132
Andrew J. Pierce <apierce@openwhere.com>
2014-10-07 12:03:52 -0400

    Add Gruntfile; Configure jshint and unit tests.

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = function(grunt) {
+    grunt.initConfig({
+        nodeunit: {
+            all: ['test/*_test.js']
+        },
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        }
+    });
+
+    // Load the tasks defined in the config above
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+    // Whenever the "test" task is run, run some basic tests.
+    var path = require('path');
+    grunt.registerTask('test', function(which) {
+        var test = path.join(__dirname, 'test', which + '.js');
+        if (grunt.file.exists(test)) {
+            grunt.config('nodeunit.tests', test);
+        }
+        grunt.task.run('nodeunit');
+    });
+
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'test']);
+};
