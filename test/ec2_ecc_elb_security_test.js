a68725a45291350ee7e6002a1ad75e7073b3e9bf
Andrew J. Pierce <apierce@openwhere.com>
2014-10-14 14:40:38 -0400

    Complex test case now correctly creates a stack in AWS

--- test/ec2_ecc_elb_security_test.js
+++ test/ec2_ecc_elb_security_test.js
@@ -18,8 +18,32 @@ exports.ec2_ecc_elb_secuirty_test = function(test){
     var filePath = '/tmp/ec2_elb_ecc_security_rds.json';
 
     // Create Parameters
-    t.strParam('EC2InstanceAMI', 'ami-123456', 'AMI Instance ID to use for the EC2 instances');
-    t.strParam('KeyName', 'test-key', 'Name of the EC2 Keypair to enable SSH access to the boxes');
+    t.strParam('CacheType', 'cache.m3.medium', 'Type of cache instance to add to the ElastiCache cluster')
+        .allowedValues([
+            'cache.m3.medium',
+            'cache.m3.large',
+            'cache.m3.xlarge',
+            'cache.m3.2xlarge',
+            'cache.r3.large',
+            'cache.r3.xlarge',
+            'cache.r3.2xlarge',
+            'cache.r3.4xlarge',
+            'cache.r3.8xlarge',
+            'cache.t2.micro',
+            'cache.t2.small',
+            'cache.t2.medium'
+        ]);
+    t.strParam('EC2InstanceType', 'c3.large', 'Type of EC2 Instance to launch')
+        .allowedValues([
+            'c3.large',
+            'c3.xlarge',
+            'c3.2xlarge',
+            'c3.4xlarge',
+            'c3.8xlarge'
+        ]);
+
+    t.strParam('EC2InstanceAMI', 'ami-6aff5e02', 'AMI Instance ID to use for the EC2 instances');
+    t.strParam('KeyName', null, 'Name of the EC2 Keypair to enable SSH access to the boxes');
     t.strParam('OpenCidrIp', '0.0.0.0/0');
 
     var webServerPort = 80;
@@ -36,8 +60,8 @@ exports.ec2_ecc_elb_secuirty_test = function(test){
         t.ec2Instance(instanceIds[j])
             .keyName( t.ref('KeyName') )
             .imageId( t.ref('EC2InstanceAMI') )
-            .name(instanceIds[j])
-            .securityGroupIds('sg1');
+            .instanceType( t.ref('EC2InstanceType') )
+            .securityGroups( t.ref('InstanceSecurityGroup') );
     }
 
     // Create the load balancer (for the ec2 instances)
@@ -49,25 +73,30 @@ exports.ec2_ecc_elb_secuirty_test = function(test){
         'Timeout': '5'
     };
     var loadBalancer = t.loadBalancer('ec2LoadBalancer')
-                        .loadBalancerName('ec2LoadBalancer')
+                        .availabilityZones()
+                        .healthCheck(lbHealth)
                         .instances(instanceIds)
-                        .healthCheck(lbHealth);
+                        .listeners({
+                          'LoadBalancerPort' : '80',
+                          'InstancePort' : { 'Ref' : 'WebServerPort' },
+                          'Protocol' : 'HTTP'
+                        });
 
     // Create Elasticache Cluster
     var eccName = 'TestECC';
     t.elastiCacheCluster(eccName)
         .autoMinorVersionUpgrade('true')
-        .cacheNodeType('cache.m3.medium')
+        .cacheNodeType( t.ref('CacheType') )
         .clusterName(eccName)
         .engine('memcached')
         .numCacheNodes(1)
-        .vpcSecurityGroupIds('sg2');
+        .vpcSecurityGroupIds( t.fnGetAtt('CacheSecurityGroup', 'GroupId') );
 
     // Create Security Groups
-    t.securityGroup('sg1', 'Security Group for the EC2 Instances')
+    t.securityGroup('InstanceSecurityGroup', 'Security Group for the EC2 Instances')
         .tcpIn({ 'CidrIp': t.ref('OpenCidrIp')}, t.ref('WebServerPort'), t.ref('RDPPort'));
-    t.securityGroup('sg2', 'Security Group for the ElastiCache Cluster')
-        .tcpIn({'SourceSecurityGroupId': t.ref('sg1')}, 11211);
+    t.securityGroup('CacheSecurityGroup', 'Security Group for the ElastiCache Cluster')
+        .tcpIn({'SourceSecurityGroupName': t.ref('InstanceSecurityGroup')}, 11211);
 
     t.save(filePath);
 
