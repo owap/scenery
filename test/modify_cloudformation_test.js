6b58ac33c8f3ec39ce2256c4eff797491dbd992e
Andrew J. Pierce <apierce@openwhere.com>
2014-10-19 19:57:43 -0400

    Initial object (re)instantiation from CloudFormation template

---
+++ test/modify_cloudformation_test.js
@@ -0,0 +1,103 @@
+'use strict';
+
+/**
+ * Create a CloudFormation template with the following:
+ *  + Two EC2 instances
+ *  + One ElastiCache cluster with a single node
+ *  + An Elastic Load Balancer
+ *  + Two security groups
+ *      - One for the EC2 instances
+ *      - One for the ElastiCache cluster
+ *  + A VPC to which the EC2 instances belong (and therefore do NOT use the security group)
+ *
+ *  Save it out to a template, and then read it back in, asserting that the
+ *  loaded template equals the one initially created. Then modify the new
+ *  template, and assert that the original values are no longer present in the
+ *  modified version.
+ **/
+
+var Template = require('../lib/Template.js');
+var Validator = require('../lib/Validator.js');
+var filePath = '/tmp/modify_cf_test.json';
+var t = new Template();
+
+exports.createOriginalTemplate = function(test){
+
+    t.strParam('EC2InstanceType', 'c3.large', 'Type of EC2 Instance to launch')
+        .allowedValues([
+            'c3.large',
+            'c3.xlarge',
+            'c3.2xlarge',
+            'c3.4xlarge',
+            'c3.8xlarge'
+        ]);
+    t.strParam('EC2InstanceAMI', 'ami-6aff5e02', 'AMI Instance ID to use for the EC2 instances');
+    t.strParam('KeyName', null, 'Name of the EC2 Keypair to enable SSH access to the boxes');
+    t.strParam('OpenCidrIp', '0.0.0.0/0');
+
+    t.elastiCacheCluster('TestECC')
+        .autoMinorVersionUpgrade('true')
+        .cacheNodeType('cache.m3.medium')
+        .engine('memcached')
+        .numCacheNodes(1);
+
+    var webServerPort = 80;
+    t.numParam('RDPPort', 3389);
+    t.numParam('WebServerPort', webServerPort);
+
+    // Create EC2 Instances
+    var instanceIds = [];
+    for(var i=0; i<2; i++){
+        instanceIds.push('loadBalacnedEc2Instance' + i);
+    }
+
+   for(var j in instanceIds){
+        t.ec2Instance(instanceIds[j])
+            .keyName( t.ref('KeyName') )
+            .imageId( t.ref('EC2InstanceAMI') )
+            .instanceType( t.ref('EC2InstanceType') );
+            //.securityGroups( t.ref('InstanceSecurityGroup') );
+    }
+
+    // Create the load balancer (for the ec2 instances)
+    var lbHealth = {
+        'Target': 'HTTP:' + webServerPort + '/',
+        'HealthyThreshold': '3',
+        'UnhealthyThreshold': '5',
+        'Interval': '30',
+        'Timeout': '5'
+    };
+    t.loadBalancer('ec2LoadBalancer')
+        .availabilityZones()
+        .healthCheck(lbHealth)
+        .instances(instanceIds)
+        .listeners({
+          'LoadBalancerPort' : '80',
+          'InstancePort' : { 'Ref' : 'WebServerPort' },
+          'Protocol' : 'HTTP'
+        });
+
+    t.save(filePath);
+
+    // Validate the template
+    test.expect(2);
+
+    function validationCallback(isValid, message){
+        if(!isValid){
+            console.log(message);
+        }
+        test.ok(isValid);
+        test.ok(!!message);
+        test.done();
+    }
+
+    var v = new Validator(filePath);
+    v.validate(validationCallback);
+};
+
+exports.testTemplateModification = function(test){
+    test.expect(1);
+    var newTemplate = Template.parse(filePath);
+    test.deepEqual(newTemplate, t);
+    test.done();
+};
