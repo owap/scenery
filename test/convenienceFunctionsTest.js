9f8ba2c56cc9350bcfa9a77e6b9b4254eb7d252e
Andrew J. Pierce <apierce@openwhere.com>
2015-03-19 14:26:19 +0000

    Add easyUserData convenience function to Instance class
    This satisfies issue #63

--- test/convenienceFunctionsTest.js
+++ test/convenienceFunctionsTest.js
@@ -23,3 +23,53 @@ exports.testConvenienceFunctionsExist = function(test) {
     });
     test.done();
 };
+
+exports.testEasyUserData = function(test) {
+    var Instance = require('../lib/EC2/Instance.js');
+
+    test.expect(5);
+    // Assert the easyUserData function exists
+    var i1 = new Instance();
+    test.ok(i1.easyUserData);
+
+    // Assert we error when we pass a non-array to easyUserData
+    var i2 = new Instance();
+    test.throws(function(){
+        i2.easyUserData('We excect an error to be thrown');
+    });
+
+    // Assert that, when we pass an array with a shebang in the first element,
+    // everything works great
+    var i3 = new Instance();
+    test.doesNotThrow(function(){
+        var commands = [
+            '#!/bin/bash -e\n',
+            'echo "valid test"'
+        ];
+        var expectedUserData =  { 
+         'Fn::Base64' : { 
+            'Fn::Join' : [ '', commands ] 
+         }
+     };
+        i3.easyUserData(commands);
+        test.deepEqual(expectedUserData, i3.node.Properties.UserData);
+    });
+
+    // Assert that, when we pass an array without a sheband in the first element,
+    // a bash invocation line is added
+    var i4 = new Instance();
+    var commands = [
+        'echo "valid test"'
+    ];
+    var expectedCommands = [
+        '#!/bin/bash -e\n',
+        'echo "valid test"'
+    ];
+    i4.easyUserData(commands);
+    test.deepEqual(
+        expectedCommands,
+        i3.node.Properties.UserData['Fn::Base64']['Fn::Join'][1]
+    );
+
+    test.done();
+};
