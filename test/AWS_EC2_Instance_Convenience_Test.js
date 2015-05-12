13f707eb6421b7ab76739437162580f230abad2b
Andrew J. Pierce <apierce@openwhere.com>
2015-05-12 18:13:14 +0000

    Make userDataFromFile fluent once more
    This commit ensures reading files remains asynchronous so as not to block
    the event loop, but makes userDataFromFile fluent by immeidatley
    returning the Instance on which we're invoking the function.
    
    Additionaly, the ability to pass a callback to the usereDataFromFile
    function has been introduced to allow users to know when the task of
    reading user data from a file has been complete. You can see how this is
    used in the AWS_EC2_Instance_Convenience_Test.js unit test.

--- test/AWS_EC2_Instance_Convenience_Test.js
+++ test/AWS_EC2_Instance_Convenience_Test.js
@@ -1,17 +1,35 @@
 'use strict';
+var fs = require('fs');
 
 exports.testUserDataFromFile = function(test) {
+    test.expect(5);
+
     var Instance = require('../lib/EC2/Instance.js');
     var i = new Instance();
-    test.expect(2);
     test.ok(i.userDataFromFile);
 
-    i.userDataFromFile(__dirname + '/userDataSample.sh').then(
-        function(instance){
-            test.ok(instance.node.Properties.UserData);
+    var callback = function(error, data) {
+        test.ok(data);
+        test.ok(!error);
+        test.ok(i.node.Properties.UserData);
+
+        var assertFileContentsEqual = function(err, data){
+            if(err){
+                throw('Test failed to assert file contents are equal:', err);
+            }
+
+            var fileContents = data.toString().split(/(\n)/g);
+            test.deepEqual(
+                fileContents,
+                i.node.Properties.UserData['Fn::Base64']['Fn::Join'][1]
+            );
             test.done();
-        }
-    );
+        };
+
+        fs.readFile(__dirname + '/userDataSample.sh', assertFileContentsEqual);
+    };
+
+    i.userDataFromFile(__dirname + '/userDataSample.sh', callback);
 };
 
 // TODO: We may also wish to add tests asserting that the UserData
