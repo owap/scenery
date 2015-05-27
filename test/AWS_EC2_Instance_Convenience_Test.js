cc9de58732a2d74092676df5a1c1c43e630ca9ba
Andrew J. Pierce <apierce@openwhere.com>
2015-05-27 18:09:53 +0000

    Promisify userDataFromFile; add associated tests

--- test/AWS_EC2_Instance_Convenience_Test.js
+++ test/AWS_EC2_Instance_Convenience_Test.js
@@ -2,6 +2,56 @@
 var fs = require('fs');
 
 exports.testUserDataFromFile = function(test) {
+    test.expect(3);
+
+    var Instance = require('../lib/EC2/Instance.js');
+    var i = new Instance();
+    test.ok(i.userDataFromFile);            // Assert the function exists
+    test.ok(!i.node.Properties.UserData);   // Assert we don't already have data
+
+    // Fetch the data
+    i.userDataFromFile(__dirname + '/userDataSample.sh').then(function(){
+        test.ok(i.node.Properties.UserData);    // Assert we now have data
+        test.done();                            // Wrap up the test
+    });
+};
+// TODO: We may also wish to add tests asserting that the UserData
+// section matches the contents of the file passed to the function.
+
+exports.testUserDataFromInvalidFile = function(test) {
+    test.expect(3);
+    var Instance = require('../lib/EC2/Instance.js');
+    var i = new Instance();
+    test.ok(i.userDataFromFile);            // Assert the function exists
+    test.ok(!i.node.Properties.UserData);   // Assert we don't already have data
+
+    // Fetch the data
+    i.userDataFromFile(__dirname + '/invalidFile.sh').then(function(){
+        test.ok(!i.node.Properties.UserData);   // Assert we still have no data
+        test.done();                            // Wrap up the test
+    });
+};
+
+exports.testUserDataFromFileIsFluent = function(test) {
+    test.expect(5);
+    var Instance = require('../lib/EC2/Instance.js');
+    var i = new Instance();
+    test.ok(i.userDataFromFile);            // Assert the function exists
+    test.ok(!i.node.Properties.UserData);   // Assert we don't already have data
+
+    i.KeyName('TestKey')
+    .ImageId('TestImageId')
+    .userDataFromFile(__dirname + '/userDataSample.sh').then(function(){
+        test.ok(i.node.Properties.KeyName);
+        test.ok(i.node.Properties.ImageId);
+        test.ok(i.node.Properties.UserData);    // Assert we now have data
+        test.done();                            // Wrap up the test
+    });
+    // TODO: This is not fluent, unfortunately, because Promises.
+    // We'll need to wait until ES7 and `await`
+};
+
+exports.testUserDataFromFileCallbackVer = function(test) {
     test.expect(5);
 
     var Instance = require('../lib/EC2/Instance.js');
@@ -25,12 +75,8 @@ exports.testUserDataFromFile = function(test) {
             );
             test.done();
         };
-
         fs.readFile(__dirname + '/userDataSample.sh', assertFileContentsEqual);
+
     };
-
-    i.userDataFromFile(__dirname + '/userDataSample.sh', callback);
+    i.userDataFromFileCallback(__dirname + '/userDataSample.sh', callback);
 };
-
-// TODO: We may also wish to add tests asserting that the UserData
-// section matches the contents of the file passed to the function.
